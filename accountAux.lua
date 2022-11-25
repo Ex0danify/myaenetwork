afe0c8bd63c26be517577c00a81d7b484b32f3c0
Ex0danify <51802014+Ex0danify@users.noreply.github.com>
2022-11-25 22:25:12 +0100

    Update accountAux.lua

--- accountAux.lua
+++ accountAux.lua
@@ -3,7 +3,7 @@ local internet = require("internet")
 local filesystem = require("filesystem")
 local shell = require("shell")
 
-local urlAccount = "http://myaenetwork.ovh/accountCreation"
+local urlAccount = "http://192.168.3.30:5000/accountCreation"
 local webIdPath = "/home/myaenetwork/webIdentification.txt"
 local workDirectory = "/home/myaenetwork/"
 local newDirectory = "/home/myaenetwork"
